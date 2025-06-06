9a03b8e6a402aa342f00b4c172b39fdbb981e43f
Leo <leo.z.chen2012@gmail.com>
2025-06-05 17:33:26 -0700

    Walljump
    Added Walljumping off of a wallrun
    Added Camera effects on wallrun

--- Assets/Scripts/Camera/PlayerCam.cs
+++ Assets/Scripts/Camera/PlayerCam.cs
@@ -1,27 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerCam : MonoBehaviour
 {
-    [Header("Sensitivity")]
     public float sensX;
     public float sensY;
-    [Header("Orentation")]
+
     public Transform orientation;
+    public Transform camHolder;
+
     float xRotation;
     float yRotation;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //get mouse input
+        // get mouse input
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
@@ -30,8 +31,18 @@ public class PlayerCam : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //rotate camera and orientation
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        // rotate cam and orientation
+        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    public void DoFov(float endValue)
+    {
+        GetComponent<Camera>().DOFieldOfView(endValue, 0.25f);
+    }
+
+    public void DoTilt(float zTilt)
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, zTilt), 0.25f);
     }
 }
